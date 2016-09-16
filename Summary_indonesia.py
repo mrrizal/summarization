369320c9c8102fb93771a767142554ab8094c93c
mrrizal <rizalubuntuuser@gmail.com>
2016-09-16 08:49:16 +0700

    update summary indonesia

--- Summary_indonesia.py
+++ Summary_indonesia.py
@@ -1,14 +1,13 @@
 import nltk.data
-#from nltk.stem import LancasterStemmer, PorterStemmer
+# from nltk.stem import LancasterStemmer, PorterStemmer
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
 from sklearn.preprocessing import Normalizer
 import pandas as pd
-import math
 from numpy import linalg, sqrt
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
-from stem import IndonesianStemmer 
+# from stem import IndonesianStemmer 
 
 class  Summary:
 
@@ -22,15 +21,15 @@ class  Summary:
 		sentences = tokenizer.tokenize(paragraph)
 		return sentences
 
-	#stemming
+	# stemming
 	def stemmed_words(self, doc):
 		factory = StemmerFactory()
 		stemmer = factory.create_stemmer()
-		#lebih cepat, tapi akurasi nya kurang
-		#stemmer = IndonesianStemmer() 
+		# lebih cepat, tapi akurasi nya kurang
+		# stemmer = IndonesianStemmer() 
 		return (stemmer.stem(w) for w in self.stopWordIndonesia(doc))
 
-	#remove stop word (indonesia stop word)
+	# remove stop word (indonesia stop word)
 	def stopWordIndonesia(self, doc):
 		analyzer = CountVectorizer().build_analyzer()
 		f = open('data/stopwords_id.txt')
@@ -52,13 +51,11 @@ class  Summary:
 			self.vectorize = TfidfVectorizer(min_df=0.0, analyzer=self.stemmed_words, max_df=1.0)
 		
 		dtm = self.vectorize.fit_transform(sentences)
-		#print(pd.DataFrame(dtm.toarray(), index=sentences, columns=self.vectorize.get_feature_names()))
-		print(len(self.vectorize.get_feature_names()))
-		print()
-		#print()
+		# print(pd.DataFrame(dtm.toarray(), index=sentences, columns=self.vectorize.get_feature_names()))
+		# print()
 		return dtm
 		
-	#create svd	
+	# create svd	
 	def getSVD(self, documentTermMatrix, sentences):
 		try :
 			lsa = TruncatedSVD(len(sentences), algorithm='randomized')
@@ -67,7 +64,7 @@ class  Summary:
 
 		dtm_lsa = lsa.fit_transform(documentTermMatrix)
 		dtm_lsa = Normalizer(copy=False).fit_transform(dtm_lsa)
-		u = lsa.components_
+		u = lsa.components_.T
 		sigma = lsa.explained_variance_ratio_
 		vt = dtm_lsa.T
 		return u, sigma, vt
@@ -93,9 +90,9 @@ class  Summary:
 		self.aspectRatio = int((aspectRatio/100)*len(vt))
 
 		if(len(vt)>3):
-			#metode milik Gong dan Liu
+			# metode milik Gong dan Liu
 			if approach == 'GongLiu':
-				#banyaknya kalimat yang di ambil
+				# banyaknya kalimat yang di ambil
 				for i in range(self.aspectRatio): 
 					maxvalue = 0
 					index = 0
@@ -109,23 +106,23 @@ class  Summary:
 					vt[index][index1] = 0
 					value[index1] = maxvalue
 
-			#metode milik Steinberger dan Jezek
+			# metode milik Steinberger dan Jezek
 			elif approach == 'SteinbergerJezek':
 				for i in range(len(vt)):
 					vt[i].append(sqrt(sum([vt[i][j]*sigma[i] for j in range(len(vt[i]))])))
 
-				#print(pd.DataFrame(vt, index=[i+1 for i in range(len(vt))], columns=[i+1 for i in range(len(vt)+1)]))
+				# print(pd.DataFrame(vt, index=[i+1 for i in range(len(vt))], columns=[i+1 for i in range(len(vt)+1)]))
 
 				data = []
 				for i in range(len(vt)):
 					data.append(vt[i][len(vt[i])-1])
 					
-				#banyaknya kalimat yang di ambil
+				# banyaknya kalimat yang di ambil
 				for i in range(self.aspectRatio): 
 					value[data.index(max(data))] = max(data)
 					data[data.index(max(data))] = 0
 
-			#copied from summy
+			# copied from summy
 			elif approach == 'SteinbergerJezek2':
 				MIN_DIMENSIONS = 3
 				REDUCTION_RATIO = 1/1
@@ -135,13 +132,13 @@ class  Summary:
 
 				for column_vector in vt:
 					tmp = sum(s*v**2 for s, v in zip(powered_sigma, column_vector))
-					data.append(math.sqrt(tmp))
+					data.append(sqrt(tmp))
 				
 				for i in range(self.aspectRatio): 
 					value[data.index(max(data))] = max(data)
 					data[data.index(max(data))] = 0
 
-			#metode cross 
+			# metode cross 
 			elif approach == 'cross':
 				lengthOfSentences = [0 for i in range(len(vt))]
 				for i in range(len(vt)):
@@ -152,14 +149,14 @@ class  Summary:
 					vt[i].append(average)
 					tmp = vt[i][:-1]
 					lengthOfSentences = [sum(y) for y in zip([tmp[x]*sigma[x] for x in range(len(tmp))], lengthOfSentences)]
-					#print(vt[i])
+					# print(vt[i])
 
 				vt.append(lengthOfSentences)
-				#for i in range(len(vt)):
-				#	print(vt[i])
+				# for i in range(len(vt)):
+				# 	print(vt[i])
 				data = vt[len(vt)-1]
 
-				#banyak kalimat yang di pilih
+				# banyak kalimat yang di pilih
 				for i in range(self.aspectRatio):
 					value[data.index(max(data))] = max(data)
 					data[data.index(max(data))] = 0
@@ -174,27 +171,27 @@ class  Summary:
 		evaluation = []
 		for i in range(len(ue)):
 			try:
-				evaluation.append(ue[i]*uf[i])
+				evaluation.append(abs(ue[i]*uf[i]))
 			except IndexError:
-				evaluation.append(0)
+				pass
 
-		return math.acos(sum(evaluation))
+		return sum(evaluation) 
 	
 		
-#main program
+# main program
 def main():
-	file = open('data/teks_indonesia_1.txt', 'r')
+	file = open('data/teks_indonesia.txt', 'r')
 	data = file.read()
 
 	summary = Summary()
-	#paragraph = summary.getParagraph(data)
+	# paragraph = summary.getParagraph(data)
 
-	#for i in paragraph:
+	# for i in paragraph:
 	sentences = summary.getSentence(data)
 	dtm = summary.getDTM(sentences, mode='CountVectorizer')
 	u, sigma, vt = summary.getSVD(dtm, sentences)
 	
-	#menampilkan hasil summary
+	# menampilkan hasil summary
 	keys = summary.getSummary(sigma=sigma, vt=vt.tolist(), approach='SteinbergerJezek2').keys()
 	keys = sorted(keys)
 	summaryResult = []
@@ -207,13 +204,18 @@ def main():
 	for i in summaryResult:
 		print(i)
 
-	'''
-	menampilkan hasil evaluasi(lsa-based)
-	dtmSummary = summary.getDTM(summaryResult, mode='CountVectorizer', binaryMode=True)
+	print()
+	print("Panjang kalimat : %i " % len(sentences))
+	print("Matrix sigma : %i " % (len(sigma)))
+	print("Matrix U(mxn) :  %i x %i " % (len(u), len(u[0])))
+
+	#  menampilkan hasil evaluasi(lsa-based)
+	dtmSummary = summary.getDTM(summaryResult, mode='CountVectorizer')
 	uSummary, sigmaSummary, vtSummary = summary.getSVD(dtmSummary, summaryResult)
 	evaluation = summary.getEvaluation(sorted(u[0]), sorted(uSummary[0]))
 	print("Evaluation : ", evaluation) 
-	'''
+	
+	
 	
 	print()
 
